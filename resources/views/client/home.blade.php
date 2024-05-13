cbda5c40de545551f78c095badc4154008ac7608
schmev <86550672+schmev91@users.noreply.github.com>
2024-05-13 12:53:06 +0700

    setting up

--- resources/views/client/home.blade.php
+++ resources/views/client/home.blade.php
@@ -1,5 +1,6 @@
-@extends('layouts.client')
-
-@section('content')
-    <h1>Hello World</h1>
+@extends("layouts.client")
+@section("content")
+    <h1 class="w-fit font-medium text-blue-300 shadow-lg shadow-slate-300">
+        Hello World
+    </h1>
 @endsection
--- resources/views/layouts/client.blade.php
+++ resources/views/layouts/client.blade.php
@@ -1,36 +1,22 @@
-<!doctype html>
+<!DOCTYPE html>
 <html lang="en">
+    <head>
+        <meta charset="utf-8" />
+        <meta name="viewport" content="width=device-width, initial-scale=1" />
+        <title>lab1</title>
 
-<head>
-    <meta charset="utf-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1">
-    <title>lab1</title>
+        @vite("resources/css/output.css")
+    </head>
 
-    @vite('resources/css/bootstrap.min.css')
-    @vite('resources/css/global.css')
-
-    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.2/css/all.min.css" />
-
-</head>
-
-<body class="d-flex flex-column gap-3 justify-content-between" style="min-height: 100vh">
-    {{-- HEADER --}}
-    <header>
-        @include('client.partials.header')
-    </header>
-
-    {{-- content - START --}}
-    <div class="container">
-        @yield('content')
-    </div>
-    {{-- content - END --}}
-
-    {{-- FOOTER --}}
-    <footer>
-        @include('client.partials.footer')
-    </footer>
-
-    @vite('resources/js/bootstrap.bundle.min.js')
-</body>
-
+    <body class="">
+        {{-- HEADER --}}
+        @include("client.partials.header")
+        {{--
+            content - START
+        --}}
+        <div class="container">@yield("content")</div>
+        {{-- content - END --}}
+        {{-- FOOTER --}}
+        @include("client.partials.footer")
+    </body>
 </html>
