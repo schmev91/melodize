6c98c9c92c64bf2ca781cd2b5119cfb7dcb3765f
schmev <86550672+schmev91@users.noreply.github.com>
2024-05-24 10:33:48 +0700

    layout music-player

--- resources/views/livewire/music-player.blade.php
+++ resources/views/livewire/music-player.blade.php
@@ -1,9 +1,9 @@
 {{-- If your happiness depends on money, you will never be happy with yourself. --}}
 @persist("music-player")
     <div
-        class="container fixed bottom-0 flex items-center gap-5 border-t border-gray-300 bg-gray-100 py-2"
+        class="container fixed bottom-0 grid grid-cols-3 border-t border-gray-300 bg-gray-100 py-2"
     >
-        <div id="player-label" class="flex items-center gap-3">
+        <div id="player-label" class="col-start-1 flex items-center gap-3">
             <img
                 class="h-16 w-16 rounded-md object-cover"
                 src="{{ asset("img/default/track.png") }}"
@@ -15,16 +15,34 @@
             </div>
         </div>
 
-        <div id="player-controller" class="flex flex-col items-center gap-2">
-            <div id="ctrl-btns" class="flex items-center gap-5">
+        <div
+            id="player-controller"
+            class="col-auto flex flex-col justify-center gap-2 justify-self-center"
+        >
+            <div id="ctrl-btns" class="flex justify-center gap-5">
                 <x-player.loop />
                 <x-player.previous wire:click="test" />
                 <x-player.pause />
                 <x-player.next />
                 <x-player.shuffle />
             </div>
-            <progress class="progress w-96" value="72" max="100"></progress>
+            <div id="duration" class="flex items-center gap-2">
+                <small class="font-medium text-hypergreen">0:00</small>
+                <progress
+                    class="progress w-[27rem]"
+                    value="72"
+                    max="100"
+                ></progress>
+                <small class="textarea-ghost font-medium">3:19</small>
+            </div>
         </div>
-        <div id="player-options"></div>
+        <div
+            id="player-options"
+            class="col-start-3 flex items-center gap-5 justify-self-end"
+        >
+            <x-player.heart />
+            <x-player.volume-on />
+            <x-player.queue />
+        </div>
     </div>
 @endpersist('music-player')
